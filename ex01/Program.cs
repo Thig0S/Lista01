9cccbcb1922699f0b728e27b226d4ba11f3abf85
thig0S_ <thiagokovalski5@gmail.com>
2026-03-12 15:55:14 -0300

    exercicio02

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Crie um programa para calcular o volume de uma caixa retangular
+
+System.Console.Write("Digite o comprimento do retangulo: ");
+decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+
+System.Console.Write("Digite o altura do retangulo: ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+
+System.Console.Write("Digite o largura do retangulo: ");
+decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+decimal volume = largura * altura * comprimento;
+
+System.Console.WriteLine("O volume e: " + volume);
