1f89190bfd7a6e714ba1a528e97f58d55b5720f4
thig0S_ <thiagokovalski5@gmail.com>
2026-03-13 13:49:32 -0300

    exercicio 05

--- ex02/obj/Debug/net10.0/ex02.AssemblyInfo.cs
+++ ex02/obj/Debug/net10.0/ex02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69d9e48dcf8bf61940dfef983f10c062249c3d7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f8d778470c53969f0488ec68ec6c19f76b5a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
